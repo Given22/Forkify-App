120b0a4728b5b49be2ccbd748a687a277142be24
Given22 <71041120+Given22@users.noreply.github.com>
2022-06-02 19:44:37 +0200

    basic

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,7 @@
+import icons from '../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +15,199 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const renderSpinner = function (parentEl) {
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div>`;
+  parentEl.innerHtml = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
+const searchRecipe = async function (search) {
+  try {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes?search=${search}`
+    );
+    const { data } = await response.json();
+    const { recipes } = await data;
+    showSearchRecipe(recipes);
+    return await recipes;
+  } catch (e) {
+    alert('Error: ' + e.message);
+  }
+};
+
+const showSearchRecipe = async function (recipes) {
+  const searchResults = document.querySelector('.results');
+  searchResults.innerHTML = ``;
+
+  recipes.slice(0, 10).forEach(recipe => {
+    const markup = `
+      <li class="preview">
+        <a class="preview__link preview__link--active" href="#${recipe.id}">
+          <figure class="preview__fig">
+            <img src="${recipe.image_url}" alt="${recipe.title}" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${recipe.title}</h4>
+            <p class="preview__publisher">${recipe.publisher}</p>
+            <div class="preview__user-generated">
+              <svg>
+                <use href="${icons}#icon-user"></use>
+              </svg>
+            </div>
+          </div>
+        </a>
+      </li>
+    `;
+    searchResults.insertAdjacentHTML('afterbegin', markup);
+  });
+  console.log(recipes);
+};
+
+const showRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    // Getting the recipe from the API.
+    renderSpinner(recipeContainer);
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok)
+      throw new Error(
+        `Request failed with status ${res.status}, and ${data.message}`
+      );
+
+    let { recipe } = data.data;
+
+    console.log(recipe);
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    // render the recipe
+
+    const markup = `
+    <figure class="recipe__fig">
+      <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
+      <h1 class="recipe__title">
+        <span>${recipe.title}</span>
+      </h1>
+    </figure>
+
+    <div class="recipe__details">
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="${icons}#icon-clock"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--minutes">${
+          recipe.cookingTime
+        }</span>
+        <span class="recipe__info-text">minutes</span>
+      </div>
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="${icons}#icon-users"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--people">${
+          recipe.servings
+        }</span>
+        <span class="recipe__info-text">servings</span>
+  
+        <div class="recipe__info-buttons">
+          <button class="btn--tiny btn--increase-servings">
+            <svg>
+              <use href="${icons}#icon-minus-circle"></use>
+            </svg>
+          </button>
+          <button class="btn--tiny btn--increase-servings">
+            <svg>
+              <use href="${icons}#icon-plus-circle"></use>
+            </svg>
+          </button>
+        </div>
+      </div>
+  
+      <div class="recipe__user-generated">
+        <svg>
+          <use href="${icons}#icon-user"></use>
+        </svg>
+      </div>
+      <button class="btn--round">
+        <svg class="">
+          <use href="${icons}#icon-bookmark-fill"></use>
+        </svg>
+      </button>
+    </div>
+      
+    <div class="recipe__ingredients">
+      <h2 class="heading--2">Recipe ingredients</h2>
+      <ul class="recipe__ingredient-list">
+        ${recipe.ingredients
+          .map(recipe => {
+            return `
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="${icons}#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">${recipe.quantity || ''}</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">${recipe.unit || ''}</span>
+                ${recipe.description}
+              </div>
+            </li>`;
+          })
+          .join('')}
+      </ul>
+    </div>
+    
+    <div class="recipe__directions">
+      <h2 class="heading--2">How to cook it</h2>
+      <p class="recipe__directions-text">
+        This recipe was carefully designed and tested by
+        <span class="recipe__publisher">${
+          recipe.publisher
+        }</span>. Please check out
+        directions at their website.
+      </p>
+      <a
+        class="btn--small recipe__btn"
+        href="${recipe.sourceUrl}"
+        target="_blank"
+      >
+        <span>Directions</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </div>
+    `;
+
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+
+    return recipe;
+  } catch (e) {
+    alert(e.message);
+  }
+};
+searchRecipe('tomato');
+// .then(r => console.log(r))
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, showRecipe)
+);
